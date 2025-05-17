dae5580483706e5342d377d92a0aef90e962ab22
lhnhidev <lhnhi420@gmail.com>
2025-05-17 19:13:42 +0700

    config public and sass

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
-import express from "express";
-import morgan from "morgan";
-import { engine } from "express-handlebars";
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const { engine } = require("express-handlebars");
 
 const app = express();
+
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use(morgan("combined"));
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
+app.engine(
+  "hbs",
+  engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
 app.set("views", "./src/views");
 
 app.get("/", (req, res) => {
